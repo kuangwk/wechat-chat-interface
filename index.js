4d65e86965db257faf655fe9870242f791319c5f
kevinkuang <kevinkuang@tencent.com>
2018-01-22 19:27:47 +0800

    standard fix

--- index.js
+++ index.js
@@ -1,233 +1,221 @@
+/* eslint-env browser */
+
 (function () {
+    // 工具函数
+  function getHash (key) {
+    const match = window.location.hash.match(new RegExp(`${key}=([^&]*)`))
+    return match ? match[1] : ''
+  }
 
-    function getHash(key) {
-        const match = window.location.hash.match(new RegExp(`${key}=([^&]*)`));
-        return match ? match[1] : '';
-    }
+  function removeClass (element, className) {
+    element.classList.remove(className)
+  }
 
-    const theme = getHash('theme') || 'NEW_YEAR';
-    const CONFIG = window[theme];
+  function addClass (element, className) {
+    element.classList.add(className)
+  }
 
-    const input = document.querySelector('.js-input');
-    const wrapper = document.querySelector('.wrapper');
-    const main = document.querySelector('main');
-    const messageList = document.querySelector('.message-list');
-    const selectList = document.querySelector('.message-select');
-    const tips = document.querySelector('.cover-tips');
-    const wechatPage = document.querySelector('.wechat-page');
-    const firstPage = document.querySelector('.first-page');
+  function scrollToBottom (element, timeout) {
+    setTimeout(() => {
+      element.scrollTop = element.scrollHeight
+    }, timeout || 0)
+  }
 
+  function fadeout (element, duration) {
+    addClass(element, 'fadeout')
+    setTimeout(() => {
+      element.remove()
+    }, duration || 300)
+  }
 
-    const maxStep = CONFIG.messages.length;
-    const selectorAnimationDuration = 300;
-    let score = 0; // 总分数
-    let step = 0;
+  function strToDomObj (str) {
+    const div = document.createElement('div')
+    div.innerHTML = str
+    return div.firstChild
+  }
 
-    // util 函数
-    function hasClass(element, className) {
-        return element.classList.contains(className);
-    }
+  function isWeixinBrowser () {
+    return /micromessenger/.test(navigator.userAgent.toLowerCase())
+  }
 
-    function removeClass(element, className) {
-        element.classList.remove(className);
-    }
+  /* ----  end of 工具函数 ----- */
 
-    function addClass(element, className) {
-        element.classList.add(className);
-    }
+  // 通过 hash 获取当前主题。 因为没有模块化，暂时主题变量就暴露在 window 上
+  const theme = getHash('theme') || 'NEW_YEAR'
+  const CONFIG = window[theme]
 
+  const wrapper = document.querySelector('.wrapper')
+  const main = document.querySelector('main')
+  const messageList = document.querySelector('.message-list')
+  const selectList = document.querySelector('.message-select')
+  const tips = document.querySelector('.cover-tips')
+  const wechatPage = document.querySelector('.wechat-page')
+  const firstPage = document.querySelector('.first-page')
 
-    function scrollToBottom(element, timeout) {
-        setTimeout(() => {
-            element.scrollTop = element.scrollHeight;
-        }, timeout || 0)
-    }
+  const maxStep = CONFIG.messages.length
+  let score = 0 // 总分数
+  let step = 0
 
-    function fadeout(element, duration) {
-        addClass(element, 'fadeout');
-        setTimeout(()=> {
-            element.remove();
-        }, duration || 300);
-    }
-
-    function strToDomObj(str) {
-        const div = document.createElement('div');
-        div.innerHTML = str;
-        return div.firstChild;
-    }
-
-    function isWeixinBrowser(){
-        return /micromessenger/.test(navigator.userAgent.toLowerCase())
-    }
-
-    function changeTitle(title) {
-        document.title = title;
-    }
-    /* ----  end of util ----- */
-
-    function bindEvents() {
-
+  function bindEvents () {
         // 实现事件委托
-        selectList.addEventListener('touchend', (event) => {
-            let target = event.target;
-            const currentTarget = event.currentTarget;
-            while (target !== currentTarget) {
-                if (target.classList.contains('js-to-select')) {
-                    const currentScore = +target.getAttribute('data-score');
-                    const message = target.querySelector('.message-bubble').innerText;
-                    appendMessage('right', message);
-                    score += currentScore;
-                    nextStep();
-                    return;
-                }
-                target = target.parentNode;
-            }
-        })
-
-        document.querySelector('.icon-replay').addEventListener('touchend', (event)=> {
-            window.location.reload();
-        })
-
-        document.querySelector('.btn-continue').addEventListener('touchend', (event)=> {
-            removeClass(wechatPage, 'hide');
-            fadeout(firstPage);
-            nextStep();
-        })
-    }
-
-    function toggleSelector(isShow) {
-        let className = 'show-selector';
-        if (isShow) {
-            addClass(wrapper, className);
-            scrollToBottom(main, 400);
-        } else {
-            removeClass(wrapper, className);
+    selectList.addEventListener('touchend', (event) => {
+      let target = event.target
+      const currentTarget = event.currentTarget
+      while (target !== currentTarget) {
+        if (target.classList.contains('js-to-select')) {
+          const currentScore = +target.getAttribute('data-score')
+          const message = target.querySelector('.message-bubble').innerText
+          appendMessage('right', message)
+          score += currentScore
+          nextStep()
+          return
         }
-    }
+        target = target.parentNode
+      }
+    })
 
-    function getMessageStr(side, message) {
-        return `<div class="message-item message-item--${side}">
+    document.querySelector('.icon-replay').addEventListener('touchend', (event) => {
+      window.location.reload()
+    })
+
+    document.querySelector('.btn-continue').addEventListener('touchend', (event) => {
+      removeClass(wechatPage, 'hide')
+      fadeout(firstPage)
+      nextStep()
+    })
+  }
+
+  function toggleSelector (isShow) {
+    let className = 'show-selector'
+    if (isShow) {
+      addClass(wrapper, className)
+      scrollToBottom(main, 400)
+    } else {
+      removeClass(wrapper, className)
+    }
+  }
+
+  function getMessageStr (side, message) {
+    return `<div class="message-item message-item--${side}">
                 <img class="avatar" src="${CONFIG.avatar[side]}" alt="头像">
                 <div class="message-bubble">${message}</div>
-            </div>`;
-    }
+            </div>`
+  }
 
-    function getSelectMsgStr(messageObj) {
-        return `<div class="message-item message-item--right js-to-select" data-score=${messageObj.score}>
+  function getSelectMsgStr (messageObj) {
+    return `<div class="message-item message-item--right js-to-select" data-score=${messageObj.score}>
                 <img class="avatar" src="${CONFIG.avatar.right}" alt="头像">
                 <div class="message-bubble">${messageObj.text}</div>
-            </div>`;
-    }
+            </div>`
+  }
 
-    function appendMessage(side, message) {
-        const msgDom = strToDomObj(getMessageStr(side, message));
-        messageList.appendChild(msgDom);
-    }
+  function appendMessage (side, message) {
+    const msgDom = strToDomObj(getMessageStr(side, message))
+    messageList.appendChild(msgDom)
+  }
 
-    function changeSelectMessage(step) {
-        const messageToSelect = CONFIG.messages[step].right;
-        let str = '';
-        messageToSelect.forEach(message => {
-            str += getSelectMsgStr(message);
-        });
-        selectList.innerHTML = str;
-    }
+  function changeSelectMessage (step) {
+    const messageToSelect = CONFIG.messages[step].right
+    let str = ''
+    messageToSelect.forEach(message => {
+      str += getSelectMsgStr(message)
+    })
+    selectList.innerHTML = str
+  }
 
-    function appendLeftMessage(step) {
-        appendMessage('left', CONFIG.messages[step].left);
-    }
+  function appendLeftMessage (step) {
+    appendMessage('left', CONFIG.messages[step].left)
+  }
 
-    function getResultByScore(score) {
-        const resultMsg = CONFIG.result;
-        let result =  resultMsg[0];
-        resultMsg.every((resultObj)=> {
-            if (score >= resultObj.score) {
-                result = resultObj
-                return false;
-            }
-            return true;
-        })
-        return result;
-    }
+  function getResultByScore (score) {
+    const resultMsg = CONFIG.result
+    let result = resultMsg[0]
+    resultMsg.every((resultObj) => {
+      if (score >= resultObj.score) {
+        result = resultObj
+        return false
+      }
+      return true
+    })
+    return result
+  }
 
-    function showTips() {
-        tips.querySelector('.tips-text').innerText = `分数：${score}
-        ${resultObj.tips}`;
-        tips.classList.remove('hide');
-    }
+  function showTips (resultObj) {
+    tips.querySelector('.tips-text').innerText = `分数：${score}
+        ${resultObj.tips}`
+    removeClass(tips, 'hide')
+  }
 
-    function showResult() {
-        setTimeout(() => {
+  function showResult () {
+    setTimeout(() => {
             // 显示左边最后的对话
-            const resultObj = getResultByScore(score);
+      const resultObj = getResultByScore(score)
             // 延时 1s 显示结果窗口
-            appendMessage('left', resultObj.say);
+      appendMessage('left', resultObj.say)
             // 显示结果窗口
-            setTimeout(()=> {
-                showTips()
-            }, 1000);
-        }, 1000);
+      setTimeout(() => {
+        showTips(resultObj)
+      }, 1000)
+    }, 1000)
+  }
+
+  function nextStep () {
+    const currentStep = step
+    toggleSelector(false)
+    if (step < maxStep) {
+      setTimeout(() => {
+        changeSelectMessage(currentStep)
+      }, 300)
+      setTimeout(() => {
+        appendLeftMessage(currentStep)
+        setTimeout(() => {
+          toggleSelector(true)
+        }, 300)  // 0.3s后显示选择框
+      }, 1000) // 延时1s显示小时
+    } else {
+      showResult()
     }
+    step += 1
+  }
 
-    function nextStep() {
-        currentStep = step;
-        toggleSelector(false);
-        if (step < maxStep) {
-            setTimeout(() => {
-                changeSelectMessage(currentStep);
-            }, selectorAnimationDuration);
-            setTimeout(() => {
-                appendLeftMessage(currentStep);
-                setTimeout(()=> {
-                    toggleSelector(true);
-                }, 300);  // 0.3s后显示选择框
-            }, 1000); // 延时1s显示小时
-        } else {
-            showResult();
-        }
-        step += 1;
+  function checkBrowser () {
+    if (isWeixinBrowser()) {
+      document.body.classList.add('wechat')
     }
+  }
 
+  function hideLoading () {
+    const loading = document.querySelector('.loading')
+    fadeout(loading)
+  }
 
-    function checkBrowser() {
-        if (isWeixinBrowser()) {
-            document.body.classList.add('wechat');
-        }
-    }
+  function preloadImg (src) {
+    const img = new Image()
+    img.src = src
+  }
 
-    function hideLoading() {
-        const loading = document.querySelector('.loading');
-        fadeout(loading);
-    }
+  function preloadImages () {
+    preloadImg(CONFIG.avatar.left)
+    preloadImg(CONFIG.avatar.right)
+    preloadImg('./img/icon/replay.svg')
+    preloadImg('./img/icon/share.svg')
+  }
 
-    function preloadImg(src) {
-        const img = new Image();
-        img.src = src;
-    }
+  function setWording () {
+    document.title = CONFIG.title
+    document.querySelector('.first-page-text').innerText = CONFIG.firstPage
+    document.querySelector('.nav-name').innerText = CONFIG.name
+  }
 
-    function preloadImages() {
-        preloadImg(CONFIG.avatar.left);
-        preloadImg(CONFIG.avatar.right);
-        preloadImg('./img/icon/replay.svg');
-        preloadImg('./img/icon/share.svg');
-    }
+  function init () {
+    checkBrowser()
+    setWording()
+    bindEvents()
+    hideLoading()
+    preloadImages()
+  }
 
-    function setWording() {
-        document.title = CONFIG.title;
-        document.querySelector('.first-page-text').innerText = CONFIG.firstPage;
-        document.querySelector('.nav-name').innerText = CONFIG.name;
-    }
-
-    function init() {
-        checkBrowser();
-        setWording();
-        bindEvents();
-        hideLoading();
-        preloadImages();
-    }
-
-    window.onload = function() {
-        init();
-    }
-
+  window.onload = function () {
+    init()
+  }
 }())
--- theme2.js
+++ theme2.js
@@ -3,73 +3,73 @@ window.QUESTION = {
   firstPage: '这是在微信聊天中的头脑王者',
   name: '头脑王者',
   avatar: {
-      left: './img/question.png',
-      right: './img/boy.png',
+    left: './img/question.png',
+    right: './img/boy.png'
   },
   messages: [{
-      left: '我们常喝的【英式奶茶】中的茶一般是哪种全发酵茶类？',
-      right: [{
-          text: '绿茶',
-          score: 0
-      }, {
-          text: '黑茶。',
-          score: 0
-      }, {
-          text: '红茶',
-          score: 1
-      }]
+    left: '我们常喝的【英式奶茶】中的茶一般是哪种全发酵茶类？',
+    right: [{
+      text: '绿茶',
+      score: 0
+    }, {
+      text: '黑茶。',
+      score: 0
+    }, {
+      text: '红茶',
+      score: 1
+    }]
   }, {
-      left: '北宋后期著名婉约派一代词宗秦观字【】',
-      right: [{
-          text: '太白',
-          score: 0
-      }, {
-          text: '少游',
-          score: 1
-      }, {
-          text: '子美',
-          score: 0
-      }]
+    left: '北宋后期著名婉约派一代词宗秦观字【】',
+    right: [{
+      text: '太白',
+      score: 0
+    }, {
+      text: '少游',
+      score: 1
+    }, {
+      text: '子美',
+      score: 0
+    }]
   }, {
-      left: '下列选项中动物细胞不具有的细胞器是【】',
-      right: [{
-          text: '细胞壁',
-          score: 0
-      }, {
-          text: '细胞壁',
-          score: 1
-      }, {
-          text: '细胞质',
-          score: 0
-      }]
+    left: '下列选项中动物细胞不具有的细胞器是【】',
+    right: [{
+      text: '细胞壁',
+      score: 0
+    }, {
+      text: '细胞壁',
+      score: 1
+    }, {
+      text: '细胞质',
+      score: 0
+    }]
   }, {
-      left: '直角三角形的两个锐角和等于多少度？',
-      right: [{
-          text: '90度',
-          score: 1
-      }, {
-          text: '120度',
-          score: 0
-      }, {
-          text: '180度',
-          score: 0
-      }]
+    left: '直角三角形的两个锐角和等于多少度？',
+    right: [{
+      text: '90度',
+      score: 1
+    }, {
+      text: '120度',
+      score: 0
+    }, {
+      text: '180度',
+      score: 0
+    }]
   }],
   result: [{
-      score: 4,
-      tips: '学霸带我飞！请收下我的膝盖！',
-      say: '满分！',
+    score: 4,
+    tips: '学霸带我飞！请收下我的膝盖！',
+    say: '满分！'
   }, {
-      score: 3,
-      tips: '徘徊在90分大门口的你要加油了！早日晋升学霸！',
-      say: '差一点就完美了',
+    score: 3,
+    tips: '徘徊在90分大门口的你要加油了！早日晋升学霸！',
+    say: '差一点就完美了'
   }, {
-      score: 1,
-      tips: '请你自动退圈保命。再不修炼你就性命不保了！',
-      say: '走远了',
+    score: 1,
+    tips: '请你自动退圈保命。再不修炼你就性命不保了！',
+    say: '走远了'
   }, {
-      score: 0,
-      tips: '没得救了，回炉重造吧！大不了十八年后又是一条好汉！',
-      say: '没救了',
+    score: 0,
+    tips: '没得救了，回炉重造吧！大不了十八年后又是一条好汉！',
+    say: '没救了'
   }]
 }
